86a549d2da9b5271f4c46dee20dabafe43b6a5df
russoturisto <russoturisto@gmail.com>
2021-02-12 09:56:13 -0700

    Updated server code (yet to be tested).

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import dotenv      from 'dotenv'
-import {fastify}   from 'fastify'
-import fastifyCors from 'fastify-cors'
-import fs          from 'fs'
+import dotenv         from 'dotenv'
+import {fastify}      from 'fastify'
+import fastifyCors    from 'fastify-cors'
+import fs             from 'fs'
+import {v4 as uuidv4} from 'uuid'
 
 dotenv.config()
 
@@ -34,14 +35,89 @@ server.register(fastifyCors, {
 	}
 })
 
-interface Game {
-	player1: string;
-	player2: string;
-	whosMove: string;
-	board: string[][];
+interface Player {
+	name: string;
+	id: string;
 }
 
-var ALL_GAMES: Game[] = []
+enum GameState {
+	PENDING,
+	STARTED,
+	FINISHED
+}
+
+interface Game {
+	board: boolean[][];
+	id: string;
+	lastMoveTime: number;
+	xMovesNext: boolean;
+	players: Player[];
+	state: GameState
+}
+
+interface ErrorResponse {
+	error: string;
+}
+
+interface GameCheckRequest {
+	gameId: string;
+	playerId: string;
+}
+
+interface GameCheckResponse {
+	state: GameState;
+	yourMove: boolean;
+	board: boolean[][]
+}
+
+interface JoinRequest {
+	playerName: string;
+}
+
+interface JoinResponse {
+	gameId: string;
+	playerId: string;
+	state: GameState;
+	yourMove: boolean;
+}
+
+interface MoveRequest {
+	gameId: string;
+	playerId: string;
+	move: {
+		column: number,
+		row: number
+	}
+	state: GameState
+}
+
+interface MoveResponse {
+	state: GameState;
+	board: boolean[][];
+}
+
+var ALL_GAMES: Map<string, Game>     = new Map<string, Game>()
+var GAMES_WITH_PLAYER_NEEDED: Game[] = []
+
+setInterval(() => {
+	const gamesToRemove: Game[] = []
+	const earliestValidGameMove = new Date().getTime() - 60 * 60 * 1000
+	for (const [gameId, game] of ALL_GAMES) {
+		if (game.lastMoveTime < earliestValidGameMove) {
+			gamesToRemove.push(game)
+		}
+	}
+	gamesToRemove.forEach(game => {
+		ALL_GAMES.delete(game.id)
+		for (var i = GAMES_WITH_PLAYER_NEEDED.length; i >= 0; i--) {
+			const currentGame = GAMES_WITH_PLAYER_NEEDED[i]
+			if (game.id === currentGame.id) {
+				GAMES_WITH_PLAYER_NEEDED.splice(i, 1)
+				break
+			}
+		}
+	})
+}, 60000)
 
 var WINNING_CONDITIONS = [
 	[{row: 0, column: 0}, {row: 1, column: 0}, {row: 2, column: 0}],
@@ -54,136 +130,180 @@ var WINNING_CONDITIONS = [
 	[{row: 0, column: 2}, {row: 1, column: 1}, {row: 0, column: 2}]
 ]
 
-server.put('/api/join', async (
+server.put('/tic-tac-toe/check', async (
 	request,
 	reply
 ) => {
-	console.log(request.body)
-	const body: any = request.body
+	const body: GameCheckRequest = request.body as any
 
-	var lastGame: Game
-	if (ALL_GAMES.length) {
-		lastGame = ALL_GAMES[ALL_GAMES.length - 1]
-		if (!lastGame.player2) {
-			lastGame.player2 = body.playerName
-			return {
-				gameIndex: ALL_GAMES.length - 1,
-				whosMove: lastGame.player1,
-				otherPlayer: lastGame.player1
-			}
-		} else {
-			lastGame = {
-				player1: body.playerName,
-				player2: null,
-				whosMove: body.playerName,
-				board: [[null, null, null], [null, null, null], [null, null, null]]
-			}
-			ALL_GAMES.push(lastGame)
-			var gameIndex = ALL_GAMES.length - 1
-			await wait()
-			return {
-				gameIndex,
-				whosMove: lastGame.player1,
-				otherPlayer: lastGame.player2
-			}
-		}
-	} else {
-		lastGame = {
-			player1: body.playerName,
-			player2: null,
-			whosMove: body.playerName,
-			board: [[null, null, null], [null, null, null], [null, null, null]]
-		}
-		ALL_GAMES.push(lastGame)
-		var gameIndex = ALL_GAMES.length - 1
-		await wait()
+	const game = ALL_GAMES.get(body.gameId)
+
+	if (!game) {
 		return {
-			gameIndex,
-			whosMove: lastGame.whosMove,
-			otherPlayer: lastGame.player2
+			error: 'Invalid request'
+		} as ErrorResponse
+	}
+
+	var player: Player
+	var playerIndex: number
+
+	for (var i = 0; i < game.players.length; i++) {
+		if (game.players[i].id === body.playerId) {
+			player      = game.players[i]
+			playerIndex = i
+			break
 		}
+	}
+
+	if (!player) {
+		return {
+			error: 'Invalid request'
+		} as ErrorResponse
+	}
+
+	if (game.state === GameState.FINISHED) {
+		ALL_GAMES.delete(game.id);
+	}
+
+	return {
+		board: game.board,
+		state: game.state,
+		yourMove: game.xMovesNext ? !playerIndex : !!playerIndex,
+	} as GameCheckResponse
+
+})
+
+server.put('/tic-tac-toe/join', async (
+	request,
+	reply
+) => {
+	if (ALL_GAMES.size > 10000) {
+		return {
+			error: 'Too many games going on, please wait'
+		} as ErrorResponse
+	}
+	const body: JoinRequest = request.body as any
+
+	if (!body.playerName || typeof body.playerName !== 'string'
+		|| !body.playerName.length) {
+		return {
+			error: 'Invalid request'
+		}
+	}
+
+	var game: Game
+	if (GAMES_WITH_PLAYER_NEEDED.length) {
+		game               = GAMES_WITH_PLAYER_NEEDED.shift()
+		var player: Player = {
+			id: uuidv4(),
+			name: body.playerName
+		}
+		game.players.push(player)
+		game.state = GameState.STARTED
+		return {
+			gameId: game.id,
+			playerId: player.id,
+			state: game.state,
+			yourMove: false
+		} as JoinResponse
+	} else {
+		game = {
+			board: [[null, null, null], [null, null, null], [null, null, null]],
+			id: uuidv4(),
+			lastMoveTime: new Date().getTime(),
+			xMovesNext: true,
+			players: [{
+				id: uuidv4(),
+				name: body.playerName
+			}],
+			state: GameState.PENDING
+		}
+		ALL_GAMES.set(game.id, game)
+		GAMES_WITH_PLAYER_NEEDED.push(game)
+
+		return {
+			gameId: game.id,
+			playerId: player.id,
+			state: game.state,
+			yourMove: true
+		} as JoinResponse
 	}
 })
 
-async function wait() {
-	return new Promise<void>((
-		resolve,
-		reject
-	) => {
-		setTimeout(() => {
-			if (ALL_GAMES[ALL_GAMES.length - 1].player2) {
-				resolve()
-			}
-		}, 1000)
-	})
-}
-
-server.put('/api/move', async (
+server.put('/tic-tac-toe/move', async (
 	request,
 	reply
 ) => {
-	const body: any = JSON.parse(request.body as any)
+	const body: MoveRequest = request.body as any
 
 	if (!body.move || typeof body.move.row !== 'number' || body.move.row < 0 || body.move.row > 2) {
 		return {
-			error: 'Invalid move'
+			error: 'Invalid request'
 		}
 	}
 
 	if (typeof body.move.column !== 'number' || body.move.column < 0 || body.move.column > 2) {
 		return {
-			error: 'Invalid move'
+			error: 'Invalid request'
 		}
 	}
 
-	if (typeof body.gameIndex !== 'number' || body.gameIndex < 0 || body.gameIndex >= ALL_GAMES.length) {
+	var game = ALL_GAMES.get(body.gameId)
+
+	if (!game) {
 		return {
-			error: 'Invalid move'
+			error: 'Invalid request'
 		}
 	}
-	var game = ALL_GAMES[body.gameIndex]
 
-	if (body.playerName !== game.whosMove) {
+	if (game.state !== GameState.STARTED) {
 		return {
-			error: 'Invalid move'
+			error: 'Invalid request'
 		}
 	}
 
-	if (game.board[body.move.row][body.move.column]) {
+	var player: Player
+	var playerIndex: number
+	for (let i = 0; i < game.players.length; i++) {
+		if (game.players[i].id === body.playerId) {
+			player      = game.players[i]
+			playerIndex = i
+		}
+	}
+
+	if (!player) {
 		return {
-			error: 'Invalid move'
+			error: 'Invalid request'
 		}
 	}
 
-	game.board[body.move.row][body.move.column] = game.whosMove
+	if ((playerIndex === 0 && !game.xMovesNext)
+		|| (playerIndex === 1 && !game.xMovesNext)) {
+		return {
+			error: 'It\'s not your move'
+		}
+	}
+
+	if (game.board[body.move.row][body.move.column] !== null) {
+		return {
+			error: 'That cell is already taken'
+		}
+	}
+
+	game.board[body.move.row][body.move.column] = game.xMovesNext
+
+	game.xMovesNext = !game.xMovesNext
 
 	var winningPlayer = whoWon(game)
 
-	if (winningPlayer) {
-		return {
-			gameOver: true,
-			winningPlayer
-		}
-	}
-
-	if (game.whosMove === game.player1) {
-		game.whosMove = game.player2
-	} else {
-		game.whosMove = game.player1
+	if (winningPlayer !== null) {
+		game.state = GameState.FINISHED
 	}
 
 	return {
-		whosMove: game.whosMove
-	}
-})
-
-server.put('/api/getGameState', async (
-	request,
-	reply
-) => {
-	const body: any = JSON.parse(request.body as any)
-
-	return ALL_GAMES[body.gameIndex]
+		state: game.state,
+		board: game.board
+	} as MoveResponse
 })
 
 function whoWon(game: Game) {
@@ -215,9 +335,8 @@ function whoWonACondition(
 // Run the server!
 const startFunction = async () => {
 	try {
-		console.log('before start tic-tac-toe')
 		await server.listen(8080, '0.0.0.0')
-		server.log.info(`server listening on ${(server.server as any).address().port}`)
+		console.log('Started tic-tac-toe server')
 	} catch (err) {
 		server.log.error(err)
 		console.log(err)
